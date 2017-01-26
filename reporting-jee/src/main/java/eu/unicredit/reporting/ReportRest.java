a7ad7feb57a84efded5ffd81bc7e40cc726b4252
c314668 <Alexlipa91>
2017-01-26 15:18:01 +0100

    new jee report structure and logging policy

--- reporting-jee/src/main/java/eu/unicredit/reporting/ReportRest.java
+++ reporting-jee/src/main/java/eu/unicredit/reporting/ReportRest.java
@@ -4,8 +4,12 @@ package eu.unicredit.reporting;
  * Created by c314668 on 19/01/17.
  */
 
+import eu.unicredit.entities.PageInfo;
 import eu.unicredit.entities.Report;
+import eu.unicredit.entities.RestInfo;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -21,8 +25,18 @@ public class ReportRest {
     @Path("/store")
     @Consumes("application/json")
     public Response createProductInJSON(Report report) {
-        String result = report.toString();
-        logger.info(result);
-        return Response.status(201).entity(result).build();
+        StringBuilder sb = new StringBuilder();
+
+        PageInfo[] pageInfos = report.getPageInfo();
+        for(PageInfo p : pageInfos)
+            logger.info("PAGE_INFO "+p.toString());
+        sb.append("Stored ").append(pageInfos.length).append(" page analytics\n");
+
+        RestInfo[] restInfos = report.getRestInfo();
+        for(RestInfo r : restInfos)
+            logger.info("REST_INFO "+r.toString());
+        sb.append("Stored ").append(restInfos.length).append(" rest analytics\n");
+
+        return Response.status(200).entity(sb.toString()).build();
     }
 }
